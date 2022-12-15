3d831815d0257af06c651bc7dde90f7978f283ac
Abhiram R <abhiramreghu09@gmail.com>
2022-12-15 17:59:24 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,5 +2,5 @@ xhost +local:docker
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -m 8GB -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/src  -it test4
+docker run -m 8GB -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}/src:/usr/src/demoprg/test-images -it test6
 xhost -local:docker
